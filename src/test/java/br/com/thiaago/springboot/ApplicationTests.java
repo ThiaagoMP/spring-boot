589ff124836b0424bc25b3033bf6264dfeb7462f
ThiaagoMP <sheldanwhiskey@gmail.com>
2024-01-03 00:59:21 -0300

    Project created

---
+++ src/test/java/br/com/thiaago/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiaago.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
